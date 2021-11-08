987fe55014f8f6cd32a769b8331bf502c14c53b4
ii-158 <iinaotookawa@gmail.com>
2021-11-08 16:27:43 +0900

    update signup button design

--- goodbook-react/src/components/Login/Login.js
+++ goodbook-react/src/components/Login/Login.js
@@ -3,43 +3,45 @@ import Input from "../UI/Input";
 import Button from "../UI/Button";
 
 const Login = (props) => {
-	return (
-		<>
-			<section className={classes.login}>
-				<div className={classes["login-wrap"]}>
-					<div className={classes["login-left"]}>
-						<h1>goodbook</h1>
-						<p>Connect with friends and the world around you on GoodBook.</p>
-					</div>
-					<div className={classes["login-right"]}>
-						<Input
-							login="true"
-							className={"login-input"}
-							input={{
-								id: "email",
-								type: "text",
-								autofocus: "1",
-								placeholder: "Email or Phone Number",
-							}}
-						/>
-						<Input
-							login="true"
-							className={"login-input"}
-							input={{
-								id: "pass",
-								type: "password",
-								placeholder: "Password",
-							}}
-						/>
-						<Button>Log In</Button>
-						<div className={classes.remenber}>
-							<a href="/">Forgot Password?</a>
-						</div>
-						<Button onClick={props.onShowSignup}>Create New Account</Button>
-					</div>
-				</div>
-			</section>
-		</>
-	);
+  return (
+    <>
+      <section className={classes.login}>
+        <div className={classes["login-wrap"]}>
+          <div className={classes["login-left"]}>
+            <h1>goodbook</h1>
+            <p>Connect with friends and the world around you on GoodBook.</p>
+          </div>
+          <div className={classes["login-right"]}>
+            <Input
+              login="true"
+              className={"login-input"}
+              input={{
+                id: "email",
+                type: "text",
+                autofocus: "1",
+                placeholder: "Email or Phone Number",
+              }}
+            />
+            <Input
+              login="true"
+              className={"login-input"}
+              input={{
+                id: "pass",
+                type: "password",
+                placeholder: "Password",
+              }}
+            />
+            <Button>Log In</Button>
+            <div className={classes.remenber}>
+              <a href="/">Forgot Password?</a>
+            </div>
+            <Button signup={"true"} onClick={props.onShowSignup}>
+              Create New Account
+            </Button>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 export default Login;
--- goodbook-react/src/components/UI/Button.js
+++ goodbook-react/src/components/UI/Button.js
@@ -1,13 +1,13 @@
 import classes from "./Button.module.css";
 
 const Button = (props) => {
-	return (
-		<button
-			onClick={props.onClick}
-			className={`${props.className} ${classes.button}`}
-		>
-			{props.children}
-		</button>
-	);
+  return (
+    <button
+      onClick={props.onClick}
+      className={`${classes.button} ${props.signup && classes.signup}`}
+    >
+      {props.children}
+    </button>
+  );
 };
 export default Button;
