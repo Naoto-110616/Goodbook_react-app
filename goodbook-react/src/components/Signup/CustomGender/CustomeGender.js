840b733235daace4f9c836d42ac0e3e6b168a60f
Naoto-110616 <siodeitadaku@gmail.com>
2021-11-09 22:52:09 +0900

    fix custome gender

--- goodbook-react/src/components/Signup/CustomGender/CustomeGender.js
+++ goodbook-react/src/components/Signup/CustomGender/CustomeGender.js
@@ -4,15 +4,28 @@ import Input from "../../UI/Input/Input";
 import { CUSTOME_GENDER } from "../../../util/Consts";
 
 const CustomeGender = () => {
-  const custome_gender = CUSTOME_GENDER.map((data) => (
-    <option value={data.value}>{data.gender}</option>
-  ));
-  return (
-    <>
-      <Select>{custome_gender}</Select>
-      <Input input={{ id: "gender", type: "text" }} />
-    </>
-  );
+	const custome_gender = CUSTOME_GENDER.map((data) => (
+		<option value={data.value}>{data.gender}</option>
+	));
+	return (
+		<>
+			<div className={classes.select}>
+				<Select>{custome_gender}</Select>
+			</div>
+			<p className={classes["gender-message"]}>
+				Your pronoun is visible to everyone.
+			</p>
+			<Input
+				signup="ture"
+				input={{ id: "gender", type: "text", placeholder: "Gender(optional)" }}
+			/>
+			<p className={classes["gender-message"]}>
+				By clicking Sign Up, you agree to our Terms, Data Policy and Cookies
+				Policy. You may receive SMS Notifications from us and can opt out any
+				time.
+			</p>
+		</>
+	);
 };
 
 export default CustomeGender;
--- goodbook-react/src/components/Signup/Signup.js
+++ goodbook-react/src/components/Signup/Signup.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Button from "../UI/Button/Button";
 import Input from "../UI/Input/Input";
 import Modal from "../UI/Modal/Modal";
@@ -7,82 +8,96 @@ import CustomeGender from "./CustomGender/CustomeGender";
 import { MONTH, DAY, YEAR } from "../../util/Consts";
 
 const Signup = (props) => {
-  const month = MONTH.map((data) => (
-    <option value={data.value}>{data.month}</option>
-  ));
-  const day = DAY.map((data) => <option value={data}>{data}</option>);
-  const year = YEAR.map((data) => <option value={data}>{data}</option>);
+	const month = MONTH.map((data) => (
+		<option value={data.value}>{data.month}</option>
+	));
+	const day = DAY.map((data) => <option value={data}>{data}</option>);
+	const year = YEAR.map((data) => <option value={data}>{data}</option>);
 
-  return (
-    <Modal>
-      <form className={classes.signup}>
-        <div>
-          <div className={classes["signup-text"]}>
-            <div>
-              <h2>Sing Up</h2>
-              <p>It’s quick and easy.</p>
-            </div>
-            <div className={classes["signup-x"]}>
-              <i onClick={props.onClose} class="far fa-times-circle"></i>
-            </div>
-          </div>
-          <Input signup="ture" input={{ placeholder: "First name" }} />
-          <Input signup="ture" input={{ placeholder: "Last name" }} />
-          <Input
-            signup="ture"
-            input={{ placeholder: "Mobile number or email" }}
-          />
-          <Input signup="ture" input={{ placeholder: "New password" }} />
-          <p className={classes["category-name"]}>Birthday</p>
-          <div className={classes.birthday}>
-            <Select signup={true} select={{ name: "month", title: "month" }}>
-              {month}
-            </Select>
-            <Select signup={true} select={{ name: "day", title: "day" }}>
-              {day}
-            </Select>
-            <Select signup={true} select={{ name: "year", title: "year" }}>
-              {year}
-            </Select>
-          </div>
-          <p className={classes["category-name"]}>Gender</p>
-          <div className={classes.gender}>
-            <Input
-              label="Female"
-              radio={true}
-              input={{
-                id: "radio1",
-                type: "radio",
-                name: "gender",
-                value: "Female",
-              }}
-            />
-            <Input
-              label="Male"
-              radio={true}
-              input={{
-                id: "radio2",
-                type: "radio",
-                name: "gender",
-                value: "Male",
-              }}
-            />
-            <Input
-              label="Custom"
-              radio={true}
-              input={{
-                id: "radio3",
-                type: "radio",
-                name: "gender",
-                value: "Custom",
-              }}
-            />
-          </div>
-          <CustomeGender />
-          <Button>Sing Up</Button>
-        </div>
-      </form>
-    </Modal>
-  );
+	const [isOpenGender, setIsOpenGender] = useState(false);
+
+	const customeGenderOpenHandler = () => {
+		setIsOpenGender(true);
+	};
+	const customeGenderCloseHandler = () => {
+		setIsOpenGender(false);
+	};
+
+	return (
+		<Modal>
+			<form className={classes.signup}>
+				<div>
+					<div className={classes["signup-text"]}>
+						<div>
+							<h2>Sing Up</h2>
+							<p>It’s quick and easy.</p>
+						</div>
+						<div className={classes["signup-x"]}>
+							<i onClick={props.onClose} class="far fa-times-circle"></i>
+						</div>
+					</div>
+					<Input signup={true} input={{ placeholder: "First name" }} />
+					<Input signup={true} input={{ placeholder: "Last name" }} />
+					<Input
+						signup={true}
+						input={{ placeholder: "Mobile number or email" }}
+					/>
+					<Input signup={true} input={{ placeholder: "New password" }} />
+					<p className={classes["category-name"]}>Birthday</p>
+					<div className={classes.birthday}>
+						<Select signup={true} select={{ name: "month", title: "month" }}>
+							{month}
+						</Select>
+						<Select signup={true} select={{ name: "day", title: "day" }}>
+							{day}
+						</Select>
+						<Select signup={true} select={{ name: "year", title: "year" }}>
+							{year}
+						</Select>
+					</div>
+					<p className={classes["category-name"]}>Gender</p>
+					<div className={classes.gender}>
+						<Input
+							onClick={customeGenderCloseHandler}
+							label="Female"
+							radio={true}
+							input={{
+								id: "radio1",
+								type: "radio",
+								name: "gender",
+								value: "Female",
+							}}
+						/>
+						<Input
+							onClick={customeGenderCloseHandler}
+							label="Male"
+							radio={true}
+							input={{
+								id: "radio2",
+								type: "radio",
+								name: "gender",
+								value: "Male",
+							}}
+						/>
+						<Input
+							onClick={customeGenderOpenHandler}
+							label="Custom"
+							radio={true}
+							input={{
+								id: "radio3",
+								type: "radio",
+								name: "gender",
+								value: "Custom",
+							}}
+						/>
+					</div>
+					{isOpenGender && <CustomeGender />}
+					<div className={classes["btn-wrap"]}>
+						<Button signup={true}>Sing Up</Button>
+					</div>
+				</div>
+			</form>
+		</Modal>
+	);
 };
 export default Signup;
