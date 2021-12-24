36fd4332ea9fd674a11994d29bfce0bfc6a870ef
ii-158 <okawa@ideaimage.co.jp>
2021-12-24 15:58:57 +0900

    refactoring button.js

--- goodbook-react/src/components/UI/Button/Button.js
+++ goodbook-react/src/components/UI/Button/Button.js
@@ -1,11 +1,13 @@
 import classes from "./Button.module.css";
 
 const Button = (props) => {
+  const buttonClasses = `${classes.button} ${props.signup && classes.signup}`
+
   return (
     <button
       disabled={props.disabled}
       onClick={props.onClick}
-      className={`${classes.button} ${props.signup && classes.signup}`}
+      className={buttonClasses}
     >
       {props.children}
     </button>
