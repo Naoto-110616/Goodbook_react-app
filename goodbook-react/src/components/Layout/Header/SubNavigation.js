c4cdb3485a110fc19cae96516c54edee563aa2df
Naoto-110616 <siodeitadaku@gmail.com>
2022-01-18 00:04:54 +0900

    add subnavmenu

--- goodbook-react/src/components/Layout/Header/SubNavigation.js
+++ goodbook-react/src/components/Layout/Header/SubNavigation.js
@@ -1,10 +1,28 @@
 import classes from "./SubNavigation.module.css";
+import { FaUserCircle, FaFacebookMessenger } from "react-icons/fa";
+import { CgMenuGridO } from "react-icons/cg";
+import { MdNotificationsActive, MdOutlineArrowDropDown } from "react-icons/md";
 
 const SubNavigation = () => {
 	return (
 		<nav className={classes.subNav}>
 			<ul>
-				<li>test</li>
+				<li className={classes.profile}>
+					<FaUserCircle />
+					<p>Name</p>
+				</li>
+				<li className={classes.circle}>
+					<CgMenuGridO />
+				</li>
+				<li className={classes.circle}>
+					<FaFacebookMessenger />
+				</li>
+				<li className={classes.circle}>
+					<MdNotificationsActive />
+				</li>
+				<li className={classes.circle}>
+					<MdOutlineArrowDropDown />
+				</li>
 			</ul>
 		</nav>
 	);
