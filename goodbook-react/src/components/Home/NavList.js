e5dfbe60e7e6ff95bdd819b6e0a84137578fca1c
ii-158 <okawa@ideaimage.co.jp>
2022-01-24 12:17:35 +0900

    refactoring navlist

--- goodbook-react/src/components/Home/NavList.js
+++ goodbook-react/src/components/Home/NavList.js
@@ -36,196 +36,135 @@ import { SiOculus, SiFacebookgaming } from "react-icons/si";
 import { BiMessageRoundedMinus, BiMessageRoundedDetail } from "react-icons/bi";
 import { CgMediaLive } from "react-icons/cg";
 
+const navList = [
+	{
+		id: "nav1", link: "/user", icon: <CgProfile />, title: "test"
+	},
+	{
+		id: "nav2", link: "/find_friend", icon: <FaUserFriends />, title: "Find friends"
+	},
+	{
+		id: "nav3", link: "/group", icon: <TiGroupOutline />, title: "Group"
+	},
+	{
+		id: "nav4", link: "/watch", icon: <MdOutlineMonitor />, title: "Watch"
+	},
+	{
+		id: "nav5", link: "/memories", icon: <IoIosStopwatch />, title: "Memories"
+	},
+	{
+		id: "nav6", link: "/saved", icon: <IoMdBookmark />, title: "Saved"
+	},
+	{
+		id: "nav7", link: "/pages", icon: <IoIosFlag />, title: "Pages"
+	},
+	{
+		id: "nav8", link: "/events", icon: <BsCalendarEvent />, title: "Events"
+	},
+	{
+		id: "nav9", link: "/jobs", icon: <MdOutlineBusinessCenter />, title: "Jobs"
+	},
+	{
+		id: "nav10", link: "/find_friend", icon: <MdRecentActors />, title: "Find friends"
+	},
+	{
+		id: "nav11", link: "/most_recent", icon: <MdRecentActors />, title: "Most recent"
+	},
+]
+const seeMoreNavList = [
+	{
+		id: "seeMoreNav1", link: "/favorites", icon: <RiStarHalfLine />, title: "Favorites"
+	},
+	{
+		id: "seeMoreNav2", link: "/ads_manage", icon: <GoGraph />, title: "Ads Manager"
+	},
+	{
+		id: "seeMoreNav3", link: "/campus", icon: <FaSchool />, title: "Campus"
+	},
+	{
+		id: "seeMoreNav4", link: "/science", icon: <MdScience />, title: "Climate science center"
+	},
+	{
+		id: "seeMoreNav5", link: "/community_help", icon: <MdOutlineLiveHelp />, title: "Community Help"
+	},
+	{
+		id: "seeMoreNav6", link: "/covid-19", icon: <FaHeartbeat />, title: "COVID-19 Information Center"
+	},
+	{
+		id: "seeMoreNav7", link: "/pay", icon: <MdPayment />, title: "GoodBook pay"
+	},
+	{
+		id: "seeMoreNav8", link: "/fundraisers", icon: <FaHandHoldingHeart />, title: "Fundraisers"
+	},
+	{
+		id: "seeMoreNav9", link: "/gaming_video", icon: <SiFacebookgaming />, title: "Gaming video"
+	},
+	{
+		id: "seeMoreNav10", link: "/live_video", icon: <CgMediaLive />, title: "Live videos"
+	},
+	{
+		id: "seeMoreNav11", link: "/messenger", icon: <BiMessageRoundedMinus />, title: "Messenger"
+	},
+	{
+		id: "seeMoreNav12", link: "/messenger_kids", icon: <BiMessageRoundedDetail />, title: "Messenger Kids"
+	},
+	{
+		id: "seeMoreNav13", link: "/play_games", icon: <MdVideogameAsset />, title: "Play games"
+	},
+	{
+		id: "seeMoreNav14", link: "/quest", icon: <SiOculus />, title: "Quest"
+	},
+	{
+		id: "seeMoreNav15", link: "/activity", icon: <AiFillPicture />, title: "Recent ad activity"
+	},
+	{
+		id: "seeMoreNav16", link: "/weather", icon: <TiWeatherPartlySunny />, title: "Weather"
+	},
+]
 const NavList = () => {
 	const dispatch = useDispatch();
 	const toggleNavList = useSelector((state) => state.navList.navListIsVisible);
 	const toggleNavListHandler = () => {
 		dispatch(navListActions.toggle());
 	};
+	const navListBtn = navList.map(data => (
+		<li key={data.id}>
+			<NavLink to={`${data.link}`}>
+				{data.icon}
+				<p>{data.title}</p>
+			</NavLink>
+		</li>
+	))
+	const seeMoreNavListBtn = seeMoreNavList.map(data => (
+		<li key={data.id}>
+			<NavLink to={`${data.link}`}>
+				{data.icon}
+				<p>{data.title}</p>
+			</NavLink>
+		</li>
+	))
 	return (
 		<>
 			<nav className={classes.navList}>
 				<ul>
-					<li>
-						<NavLink to="user">
-							<CgProfile />
-							<p>test test</p>
-						</NavLink>
+					{navListBtn}
+					{toggleNavList && seeMoreNavListBtn}
+					<li className={classes.seeMore} onClick={toggleNavListHandler}>
+						<div>
+							{!toggleNavList && (
+								<>
+									<IoIosArrowDropdown />
+									<p>See more</p>
+								</>
+							)}
+							{toggleNavList && (
+								<>
+									<IoIosArrowDropup />
+									<p>See Less</p>
+								</>
+							)}
+						</div>
 					</li>
-					<li>
-						<NavLink to="user">
-							<FaUserFriends />
-							<p>Find friends</p>
-						</NavLink>
-					</li>
-					<li>
-						<NavLink to="user">
-							<TiGroupOutline />
-							<p>Group</p>
-						</NavLink>
-					</li>
-					<li>
-						<NavLink to="user">
-							<MdOutlineMonitor />
-							<p>Watch</p>
-						</NavLink>
-					</li>
-					<li>
-						<NavLink to="user">
-							<IoIosStopwatch />
-							<p>Memories</p>
-						</NavLink>
-					</li>
-					<li>
-						<NavLink to="user">
-							<IoMdBookmark />
-							<p>Saved</p>
-						</NavLink>
-					</li>
-					<li>
-						<NavLink to="user">
-							<IoIosFlag />
-							<p>Pages</p>
-						</NavLink>
-					</li>
-					<li>
-						<NavLink to="user">
-							<BsCalendarEvent />
-							<p>Events</p>
-						</NavLink>
-					</li>
-					<li>
-						<NavLink to="user">
-							<MdOutlineBusinessCenter />
-							<p>Jobs</p>
-						</NavLink>
-					</li>
-					<li>
-						<NavLink to="user">
-							<MdRecentActors />
-							<p>Most recent</p>
-						</NavLink>
-					</li>
-					{toggleNavList && (
-						<>
-							<li>
-								<NavLink to="user">
-									<GoGraph />
-									<p>Ads Manager</p>
-								</NavLink>
-							</li>
-							<li>
-								<NavLink to="user">
-									<FaSchool />
-									<p>Campus</p>
-								</NavLink>
-							</li>
-							<li>
-								<NavLink to="user">
-									<MdScience />
-									<p>Climate science center</p>
-								</NavLink>
-							</li>
-							<li>
-								<NavLink to="user">
-									<MdOutlineLiveHelp />
-									<p>Community Help</p>
-								</NavLink>
-							</li>
-							<li>
-								<NavLink to="user">
-									<FaHeartbeat />
-									<p>COVID-19 Information Center</p>
-								</NavLink>
-							</li>
-							<li>
-								<NavLink to="user">
-									<MdPayment />
-									<p>Goodbook Pay</p>
-								</NavLink>
-							</li>
-						</>
-					)}
-					<li>
-						<NavLink to="user">
-							<RiStarHalfLine />
-							<p>Favorites</p>
-						</NavLink>
-					</li>
-					{toggleNavList && (
-						<>
-							<li>
-								<NavLink to="user">
-									<FaHandHoldingHeart />
-									<p>Fundraisers</p>
-								</NavLink>
-							</li>
-							<li>
-								<NavLink to="user">
-									<SiFacebookgaming />
-									<p>Gaming Video</p>
-								</NavLink>
-							</li>
-							<li>
-								<NavLink to="user">
-									<CgMediaLive />
-									<p>Live videos</p>
-								</NavLink>
-							</li>
-							<li>
-								<NavLink to="user">
-									<BiMessageRoundedMinus />
-									<p>Messenger</p>
-								</NavLink>
-							</li>
-							<li>
-								<NavLink to="user">
-									<BiMessageRoundedDetail />
-									<p>Messenger Kids</p>
-								</NavLink>
-							</li>
-							<li>
-								<NavLink to="user">
-									<MdVideogameAsset />
-									<p>Play Games</p>
-								</NavLink>
-							</li>
-							<li>
-								<NavLink to="user">
-									<SiOculus />
-									<p>Quest</p>
-								</NavLink>
-							</li>
-							<li>
-								<NavLink to="user">
-									<AiFillPicture />
-									<p>Recent ad activity</p>
-								</NavLink>
-							</li>
-							<li>
-								<NavLink to="user">
-									<TiWeatherPartlySunny />
-									<p>Weather</p>
-								</NavLink>
-							</li>
-						</>
-					)}
-					{!toggleNavList && (
-						<li className={classes.seeMore} onClick={toggleNavListHandler}>
-							<div>
-								<IoIosArrowDropdown />
-								<p>See more</p>
-							</div>
-						</li>
-					)}
-					{toggleNavList && (
-						<li className={classes.seeMore} onClick={toggleNavListHandler}>
-							<div>
-								<IoIosArrowDropup />
-								<p>See Less</p>
-							</div>
-						</li>
-					)}
 				</ul>
 			</nav>
 		</>
