97a3fef0f5e9c9096980e51478cf1e85b4084cbe
Naoto-110616 <siodeitadaku@gmail.com>
2022-02-05 23:16:08 +0900

    add change password function

--- goodbook-react/src/pages/ChangePassword.js
+++ goodbook-react/src/pages/ChangePassword.js
@@ -1,7 +1,59 @@
 import Layouts from "../components/Layout/Layouts";
 
+import classes from "./ChangePassword.module.css";
+
+import Input from "../components/UI/Input/Input";
+import Form from "../components/UI/Form/Form";
+import Button from "../components/UI/Button/Button";
+
+import { useRef } from "react";
+import { useHistory } from "react-router-dom";
+import { useSelector } from "react-redux";
+
 const ChangePassword = () => {
-	return <Layouts></Layouts>;
+	const history = useHistory();
+	const newPasswordInputRef = useRef();
+	const token = useSelector((state) => state.auth.token);
+
+	const submitHandler = (event) => {
+		event.preventDefault();
+		const enteredNewPassword = newPasswordInputRef.current.value;
+		fetch(
+			"https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyBV_HDSl0eps1HRi2_oXPPseJrYlUvBzys",
+			{
+				method: "POST",
+				body: JSON.stringify({
+					idToken: token,
+					password: enteredNewPassword,
+					returnSecureToken: false,
+				}),
+				headers: {
+					"Content-Type": "application/json",
+				},
+			}
+		).then((res) => {
+			// assumption: Always succeeds!
+			history.replace("/home");
+		});
+	};
+
+	return (
+		<Layouts>
+			<Form onSubmit={submitHandler}>
+				<div className={classes.changePassword}>
+					<h2>Change Password</h2>
+					<Input
+						input={{
+							ref: newPasswordInputRef,
+							id: "new-password",
+							minLength: "7",
+						}}
+					/>
+					<Button>submit</Button>
+				</div>
+			</Form>
+		</Layouts>
+	);
 };
 
 export default ChangePassword;
